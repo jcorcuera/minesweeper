dd78ca417feac2fd26fac8d8f5ec255cbbb9fb9c
José Corcuera <jzcorcuera@gmail.com>
2017-02-04 19:29:10 -0500

    Add a queue to accumulate near tiles only if not bombs are near. #7

--- app/services/game_tile_reveal_service.rb
+++ app/services/game_tile_reveal_service.rb
@@ -12,7 +12,7 @@ class GameTileRevealService
     reveal(row, col)
   end
 
-  def reveal(_row, _col)
+  def reveal(_row, _col, queue=[])
     current_tile = game.tile_at(_row, _col)
 
     raise Game::BombExploded if current_tile == GAME::TILE[:bomb]
@@ -20,9 +20,11 @@ class GameTileRevealService
     if current_tile == Game::TILE[:closed]
       near_tiles = game.near_tiles_at(_row, _col)
       total_bombs =  near_tiles.count {|r,c| game.tile_at(r,c) == GAME::TILE[:bomb] }
-
       game.tile_set(_row, _col, total_bombs.to_s)
+      queue += near_tiles if total_bombs.zero?
     end
+
+    reveal(queue.shift, queue)
   end
 
 end
