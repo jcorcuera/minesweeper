04b3dcd5307209b728f77b0a94de1f0fad22d5af
José Corcuera <jzcorcuera@gmail.com>
2017-02-05 20:18:20 -0500

    Reduce game limits

--- app/assets/javascripts/components/game_new.js.vue
+++ app/assets/javascripts/components/game_new.js.vue
@@ -7,15 +7,15 @@
     <form v-on:submit.prevent="onSubmit">
       <div>
         <label>Numher of Rows:</label>
-        <input v-model.number="game.rows" type="number" name="rows" min="1" max="100" required>
+        <input v-model.number="game.rows" type="number" name="rows" min="1" max="30" required>
       </div>
       <div>
         <label>Numher of Cols:</label>
-        <input v-model.number="game.cols" type="number" name="rows" min="1" max="100" required>
+        <input v-model.number="game.cols" type="number" name="rows" min="1" max="30" required>
       </div>
       <div>
         <label>Numher of Mines:</label>
-        <input v-model.number="game.mines" type="number" name="mines" min="1" max="200" required>
+        <input v-model.number="game.mines" type="number" name="mines" min="1" required>
       </div>
 
       <button type="submit" value="Submit">Submit</button>
